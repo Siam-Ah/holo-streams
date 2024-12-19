757767a27f94d085bdd77d5141c362b8d5ad882a
Siam <ahmedsiam037@gmail.com>
2024-12-19 17:32:30 +1100

    fixing chat

--- src/components/ShowChat.js
+++ src/components/ShowChat.js
@@ -2,8 +2,7 @@ export default function ShowChat({ stream }) {
   return (
     <iframe
       title={stream.title}
-      // src={`https://www.youtube.com/live_chat?v=${stream.id}&embed_domain=${window.location.hostname}`}
-      src={`https://www.youtube.com/live_chat?v=${stream.id}&embed_domain=https://holo-streams.netlify.app`}
+      src={`https://www.youtube.com/live_chat?v=${stream.id}&embed_domain=${window.location.hostname}`}
     ></iframe>
   );
 }
